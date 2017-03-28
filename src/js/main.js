db9b377a1b949e09f099dfd04e3c2ab450d44c50
xCss <yy.liu@foxmail.com>
2017-03-28 22:54:25 +0800

    add new page

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    progressively.init();
+    var click = 'ontouchstart' in window ? 'touchstart' : 'click';
+    if (DEVICE.isMobile) {
+        $(document.body).addClass('mobile');
+    }
+    $(document).on(click, '.menu-btn', function(e) {
+        $('.menu,.menu-btn').toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $('.mask').fadeIn(300);
+            $(document.body).addClass('over');
+        } else {
+            $('.mask').fadeOut(300);
+            $(document.body).removeClass('over');
+        }
+    }).on(click, '.mask', function() {
+        $('.menu,.menu-btn').removeClass('active');
+        $(document.body).removeClass('over');
+        $(this).fadeOut(300);
+    }).on(click,'.ctrl',function(e){
+        e.preventDefault();
+    });
+});
